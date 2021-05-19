a684909ecca7ebd4e13731be35db77a4c643c212
marco <marco.forgione@supsi.ch>
2021-05-19 19:03:08 +0200

    checking jacobians

--- examples/minimal/extract_test.py
+++ examples/minimal/extract_test.py
@@ -6,7 +6,7 @@ from extract_util import extract_weights, load_weights
 if __name__ == "__main__":
 
     n_hidden = 16
-    bsize = 32
+    bsize = 2
     # randomly initialize a neural network
     model = torch.nn.Sequential(torch.nn.Linear(1, n_hidden),
                                 torch.nn.ReLU(),
@@ -17,12 +17,18 @@ if __name__ == "__main__":
 
 
     params, names = extract_weights(model)
+    params_dict = dict(zip(names, params))
 
     def mod2func(*new_params: Tensor) -> Tensor:
         load_weights(model, names, new_params)
         out = model(inputs)
         return out
 
-    #out = mod2func(*params)
+    out = mod2func(*params)
 
     J = torch.autograd.functional.jacobian(mod2func, params)
+    J_dict = dict(zip(names, J))
+
+    hidden = inputs.matmul(params_dict['0.weight'].t())  # + params_dict['0.bias']
+    hidden += params_dict['0.bias'].unsqueeze(0).expand_as(hidden)
+    hidden_nl = torch.relu(hidden)
--- examples/minimal/extract_test_dyno.py
+++ examples/minimal/extract_test_dyno.py
@@ -4,10 +4,12 @@ import torch
 from extract_util import extract_weights, load_weights
 
 
+# idea based on https://discuss.pytorch.org/t/get-gradient-and-jacobian-wrt-the-parameters/98240
+
 if __name__ == "__main__":
 
     bsize = 1
-    N = 100
+    N = 10000
 
     u_in = torch.randn(bsize, N, 1)
     y_out = torch.randn(bsize, N, 1)
@@ -15,8 +17,6 @@ if __name__ == "__main__":
     model = models.WHNet()
 
     out_ = model(u_in)
-
-
     params, names = extract_weights(model)
 
     def mod2func(*new_params: Tensor) -> Tensor:
@@ -25,5 +25,5 @@ if __name__ == "__main__":
         return out
 
     J = torch.autograd.functional.jacobian(mod2func, params)
-
     J_dict = dict(zip(names, J))
+
