fixed dimensionality and added plots for posterior mean and standard deviation

examples/finite_ntk_dyno_experiments/minimal.py
@@ -1,16 +1,21 @@
+import numpy as np
 import torch
 import gpytorch
 import finite_ntk
+import matplotlib.pyplot as plt
 
-data_transf = torch.randn(300, 1)
-response_transf = torch.randn(300, 1)
-data_new = torch.randn(300, 1)
+n_hidden = 100
+n_in = 10
+n_seq = 30
+n_out = 1
+
+data_transf = 0.1*torch.randn(n_seq, n_in)
+response_transf = torch.clone(data_transf[:,0]) * 3.
 
 # randomly initialize a neural network
-model = torch.nn.Sequential(torch.nn.Linear(1, 30),
+model = torch.nn.Sequential(torch.nn.Linear(n_in, n_hidden),
                             torch.nn.ReLU(),
-                            torch.nn.BatchNorm1d(30),
-                            torch.nn.Linear(30, 1))
+                            torch.nn.Linear(n_hidden, n_out))
 
 class ExactGPModel(gpytorch.models.ExactGP):
     # exact RBF Gaussian process class
@@ -26,6 +31,26 @@ class ExactGPModel(gpytorch.models.ExactGP):
 
 gp_lh = gpytorch.likelihoods.GaussianLikelihood()
 gp_model = ExactGPModel(data_transf, response_transf, gp_lh, model)
+print("GP model ", gp_model)
 
 # draw a sample from the GP with kernel given by Jacobian of model
-zeromean_pred = gp_lh(gp_model(data_transf)).sample()
+# samples = gp_lh(gp_model(data_transf)).sample()
+# gp_lh.eval()
+# gp_model.eval()
+# zeromean_pred = gp_lh(gp_model(data_transf))
+zeromean_pred = gp_model(data_transf).mean
+conf = gp_model(data_transf).stddev.mul_(3.)
+
+f, ax = plt.subplots()
+ax.plot(response_transf, "r", label="Data")
+ax.plot(zeromean_pred.detach().numpy(), "b--", label="GP predictions")
+ax.legend()
+# Plot training data as black stars
+# Shade between the lower and upper confidence bounds
+ax.plot(zeromean_pred.detach().numpy() + conf.detach().numpy(), "g--")
+ax.plot(zeromean_pred.detach().numpy() - conf.detach().numpy(), "g--")
+ax.set_ylim([-6, 6])
+ax.legend(['Observed Data', 'Mean', 'Confidence'])
+
+
+plt.show()