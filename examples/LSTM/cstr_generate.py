75e3cf73e4e03649aa553c828a844858542a821f
marco <marco.forgione@supsi.ch>
2021-08-25 17:02:19 +0200

    cstr-lstm test added

--- examples/LSTM/cstr_generate.py
+++ examples/LSTM/cstr_generate.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     system = CSTR()
 
+    # Generate train and test datasets
     batch_size = 64
     n_steps = 256
     u_train, u_test, y_train, y_test = system.generate_data(batch_size, n_steps, flow_period=50)
@@ -24,3 +25,12 @@ if __name__ == "__main__":
     np.save(os.path.join("data", "cstr", "u_test.npy"), u_test)
     np.save(os.path.join("data", "cstr", "y_train.npy"), y_train)
     np.save(os.path.join("data", "cstr", "y_test.npy"), y_test)
+
+    # Generate transfer and evaluation datasets
+    batch_size = 1
+    n_steps = 1024
+    u_transf, u_eval, y_transf, y_eval = system.generate_data(batch_size, n_steps, flow_period=50)
+    np.save(os.path.join("data", "cstr", "u_transf.npy"), u_transf)
+    np.save(os.path.join("data", "cstr", "u_eval.npy"), u_eval)
+    np.save(os.path.join("data", "cstr", "y_transf.npy"), y_transf)
+    np.save(os.path.join("data", "cstr", "y_eval.npy"), y_eval)
